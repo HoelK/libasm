a8ea197d813b02d1cdc9471edc72426f2aced49e
HoelK <KeromnesHoel@gmail.com>
2026-03-30 20:24:01 +0200

    fixed malloc fail return value

--- src/mandatory/ft_strdup.s
+++ src/mandatory/ft_strdup.s
@@ -1,5 +1,6 @@
 BITS 64
 extern _malloc
+extern __errno_location
 extern ft_strlen
 extern ft_strcpy
 
@@ -10,7 +11,18 @@ ft_strdup:
 			CALL ft_strlen
 			INC rax
 			CALL _malloc
+			CMP rax, 0
+			JL set_errno
 			MOV rsi, rdi
 			MOV rdi, rax
 			CALL ft_strcpy
+
+ret:
 			RET
+
+set_errno:
+			MOV r8, rax
+			CALL __errno_location
+			MOV [rax], r8
+			MOV rax, -1
+			JMP ret
