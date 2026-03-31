f929845e650be833376ba26520d72ef7dbf39418
HoelK <KeromnesHoel@gmail.com>
2026-03-31 18:12:58 +0200

    added helpers to header

--- header/libasm.h
+++ header/libasm.h
@@ -6,3 +6,9 @@ int		ft_strcmp(const char *s1, const char *s2);
 ssize_t	ft_write(int fd, const void *buf, size_t count);
 int		ft_read(int fd, void *buf, size_t count);
 char	*ft_strdup(const char *s);
+
+bool	ft_isdigit(char c);
+bool	ft_isspace(char c);
+bool	ft_isset(char c, const char *set);
+
+int		ft_atoi_base(const char *s);
